39e9e627052e87382eb89d5c54b03d0fb002e2c5
Hao Nguyensong <hao.nguyensong@powergatesoftware.com>
2023-04-15 18:14:09 +0700

    create pagination and get query param from url

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AsideFilter from './AsideFilter'
 import SortProductList from './SortProductList'
 import Product from './Product/Product'
 import { useQuery } from '@tanstack/react-query'
 import useQueryParams from 'src/hooks/useQueryParams'
 import productApi from 'src/services/product.api'
+import Pagination from 'src/components/Pagination'
+import { ProductListConfig } from 'src/types/product.type'
+import { isUndefined, omitBy } from 'lodash'
+
+export type QueryConfig = {
+  [key in keyof ProductListConfig]: string
+}
 
 const ProductList = () => {
-  const queryParams = useQueryParams()
+  const queryParams: QueryConfig = useQueryParams()
+  const queryConfig: QueryConfig = omitBy(
+    {
+      page: queryParams.page || '1',
+      limit: queryParams.limit,
+      sort_by: queryParams.sort_by,
+      name: queryParams.name,
+      order: queryParams.order,
+      price_max: queryParams.price_max,
+      price_min: queryParams.price_min,
+      rating_filters: queryParams.rating_filters
+    },
+    isUndefined
+  )
   const { data } = useQuery({
     queryKey: ['products', queryParams],
     queryFn: () => {
-      return productApi.getProducts(queryParams)
+      return productApi.getProducts(queryParams as ProductListConfig)
     }
   })
-  console.log(data)
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container'>
@@ -25,14 +44,15 @@ const ProductList = () => {
           <div className='col-span-9'>
             <SortProductList />
             <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-              {Array(30)
-                .fill(0)
-                .map((_, index) => (
-                  <div key={index} className='col-span-1'>
-                    <Product />
+              {data &&
+                data.data.data.products.map((product) => (
+                  <div key={product._id} className='col-span-1'>
+                    <Product product={product} />
                   </div>
                 ))}
             </div>
+
+            <Pagination queryConfig={queryConfig} pageSize={data?.data.data.pagination.page_size} />
           </div>
         </div>
       </div>
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,3 +8,13 @@ export function isAxiosError<T>(error: unknown): error is AxiosError<T> {
 export function isAxiosUnprocessableEntityError<FormError>(error: unknown): error is AxiosError<FormError> {
   return isAxiosError(error) && error.response?.status === HttpStatusCode.UnprocessableEntity
 }
+
+export function formatCurrency(current: number) {
+  return new Intl.NumberFormat('de-DE').format(current);
+}
+export function formatNumberToSocialStyle(value: number) {
+  return new Intl.NumberFormat('en', {
+    notation: 'compact',
+    maximumFractionDigits: 1
+  }).format(value)
+}
