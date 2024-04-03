924d046b32f56c9be4dcbf9c77b56543072c1807
nam.kt <nam.khuatthanh@adamosoft.com>
2024-04-03 21:05:14 +0700

    Add build output to /var/www/react-cicd/ and update login button text

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -88,7 +88,7 @@ const Login = () => {
                   type='submit'
                   className=' hover:bg-text-500 flex w-full cursor-pointer items-center justify-center bg-red-500 px-2 py-4 text-center uppercase text-white'
                 >
-                  Đằng nhập
+                  haha
                 </Button>
               </div>
               <div className='mt-8'>
